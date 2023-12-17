ac72a5856c2d4d13187627d5956890a9827c00a7
Kasee18 <mutisyavincent99@gmail.com>
2023-12-17 11:29:14 +0000

    done

--- pint_operations.c
+++ pint_operations.c
@@ -2,49 +2,51 @@
 #include <stdlib.h>
 
 #define STACK_SIZE 100
-
+typedef struct
 {
 	int stack[STACK_SIZE];
 	int top;
 } Stack;
-/**
-*initStack - entry point
-*@stack: pointer
-*Return: void
-*/
 void initStack(Stack *stack)
 {
 	stack->top = -1;
 }
-/**
-*push - program entry point
-*@stack: pointer to stack
-*@value: integer
-*@line_number: line carrying the number
-*Return: void
-*/
 void push(Stack *stack, int value, int line_number)
 {
 	if (stack->top == STACK_SIZE - 1)
-
 	{
 	fprintf(stderr, "L%d: Stack overflow\n", line_number);
 	exit(EXIT_FAILURE);
 	}
-	stack->stack[++stack->top] = value;
+	stack->stack[++stack->top]; = value;
 }
-/**
-*main - entry point
-*Return: integer
-*/
-int main(void)
+void pal(Stack *stack)
+{
+	int i;
+	for (i = stack->top; i >= 0; i--)
+	{
+	printf("%d\n", stack->stack[i]);
+	}
+}
+void pint(Stack *stack, int line_number)
+{
+	if (stack->top == -1)
+	
+	{
+	fprintf(stderr, "L%d: can't pint, empty\n",line_number);
+	exit(EXIT_FAILURE);
+	}
+	printf("%d\n", stack->stack[stack->top]);
+}
+int main (void)
 {
 	Stack stack;
-
 	initStack(&stack);
+
 	push(&stack, 10, 1);
 	push(&stack, 20, 2);
 	push(&stack, 30, 3);
-	pall(&stack);
+
+	pint(&stack, 4);
 	return (0);
 }
